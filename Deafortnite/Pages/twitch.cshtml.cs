b2278232c4a06fe4e50322cbee981d2b048818fd
LeeLu1k <119786889+LeeLu1k@users.noreply.github.com>
2023-06-12 12:39:39 +0500

    Добавьте файлы проекта.

---
+++ Deafortnite/Pages/twitch.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Deafortnite.Pages
+{
+    public class twitchModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
